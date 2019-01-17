ba21b1b0c66efb4d5f155c1eb098ce559d8ec566
Yogesh Motwani <yogeshmotwani1@gmail.com>
2019-01-17 09:58:56 +0530

    addition added

---
+++ caladd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+public class caladd{
+
+	public static void main(String[] args){
+	
+		Scanner sc = new Scanner(System.in);
+		
+		switch(choice){
+			case '1': System.out.print("addition");
+		}
+
+
+	}
+
+
+
+}
